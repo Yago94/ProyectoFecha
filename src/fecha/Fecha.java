ef1922ba7b591d656be19575340422fd0e6a535b
203p21 <203p21@203p18-PC>
2017-02-10 09:37:42 +0100

    Primer punto

---
+++ src/fecha/Fecha.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fecha;
+
+/**
+ *
+ * @author 203p21
+ */
+public class Fecha {
+    int dia;
+    int mes;
+    int anno;
+    
+}
